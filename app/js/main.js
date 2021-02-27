32e25f637e94b1933c2c09675778f1b119d42493
Dencho96 <denysvk.96@gmail.com>
2021-02-27 19:04:24 +0200

    added offer section

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+const offerSlider = new Swiper('.offer__slider', {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.offer__btn--next',
+        prevEl: '.offer__btn--prev',
+    },
+});
